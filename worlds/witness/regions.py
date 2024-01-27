4ae7b0a3985145a00c871dd0f5aedd212551865e
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-01-27 02:39:10 +0100

    Merging being hard is hard ok

--- worlds/witness/regions.py
+++ worlds/witness/regions.py
@@ -117,7 +117,8 @@ class WitnessRegions:
                 self.connect_if_possible(world, region_name, connection[0], connection[1], regions_by_name)
 
         self.created_regions = {k: v for k, v in regions_by_name.items()}
-        world.multiworld.regions += self.created_regions
+
+        world.multiworld.regions += self.created_regions.values()
 
     def __init__(self, locat: WitnessPlayerLocations, world: "WitnessWorld"):
         difficulty = world.options.puzzle_randomization
