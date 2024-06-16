47c44607cbdff53016d97def9799ae9d25a68fc8
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-06-16 08:31:11 +0200

    .

--- Main.py
+++ Main.py
@@ -429,6 +429,7 @@ def main(args, seed=None, baked_server_options: Optional[Dict[str, object]] = No
         # an empty state and an all state
         for player in multiworld.player_ids:
             all_state = multiworld.get_all_state(False)
+            all_state.sweep_for_events()
             for entrance in multiworld.get_entrances(player):
                 if entrance.access_rule:
                     entrance.access_rule(all_state)
